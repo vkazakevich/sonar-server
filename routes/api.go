0d742fb98c5b671f9521aa4a7705e527b28872a7
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-05-05 16:33:28 +0200

    Clear routes

--- routes/api.go
+++ routes/api.go
@@ -13,20 +13,6 @@ func ApiRoutes(e *echo.Echo, с *controllers.Controller) {
 	product.PUT("/:id", с.UpdateProduct)
 	product.DELETE("/:id", с.DeleteProduct)
 
-	cart := e.Group("/cart")
-	cart.GET("", с.GetAllCartItems)
-	cart.POST("", с.CreateCartItem)
-	cart.GET("/:id", с.FindCartItem)
-	cart.PUT("/:id", с.UpdateCartItem)
-	cart.DELETE("/:id", с.DeleteCartItem)
-
-	cat := e.Group("/categories")
-	cat.GET("", с.GetAllCategories)
-	cat.POST("", с.CreateCategory)
-	cat.GET("/:id", с.FindCategory)
-	cat.PUT("/:id", с.UpdateCategory)
-	cat.DELETE("/:id", с.DeleteCategory)
-
 	p := e.Group("/payments")
 	p.POST("", с.CreatePayment)
 }
